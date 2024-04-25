56baab3291cdc90019687feb92b7a795d7a28a58
talhayi <talhayldrm23@gmail.com>
2024-04-26 00:39:41 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    repositories {
+        google()
+    }
+}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21" apply false
+}
