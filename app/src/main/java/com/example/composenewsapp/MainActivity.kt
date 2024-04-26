e31aa4a1a949eff7338297da263903a485e30d4f
talhayi <talhayldrm23@gmail.com>
2024-04-26 20:29:11 +0300

    Added onboarding screen

--- app/src/main/java/com/example/composenewsapp/MainActivity.kt
+++ app/src/main/java/com/example/composenewsapp/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.composenewsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.composenewsapp.presentation.onboarding.OnBoardingScreen
 import com.example.composenewsapp.ui.theme.ComposeNewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +17,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeNewsAppTheme {
-
-
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/composenewsapp/presentation/onboarding/Page.kt
@@ -0,0 +1,27 @@
+package com.example.composenewsapp.presentation.onboarding
+
+import androidx.annotation.DrawableRes
+import com.example.composenewsapp.R
+
+data class Page(
+    val title: String,
+    val description: String,
+    @DrawableRes val image: Int
+)
+val pages = listOf(
+    Page(
+        title = "Lorem Ipsum is simply dummy",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding1
+    ),
+    Page(
+        title = "Lorem Ipsum is simply dummy",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding2
+    ),
+    Page(
+        title = "Lorem Ipsum is simply dummy",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding3
+    )
+)
