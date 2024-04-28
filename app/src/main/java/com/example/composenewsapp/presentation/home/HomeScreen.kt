bf34f53d1a26bf157533944dd400fecd5a4b00d4
talhayi <talhayldrm23@gmail.com>
2024-04-29 01:47:46 +0300

    Created HomeScreen

---
+++ app/src/main/java/com/example/composenewsapp/presentation/home/HomeScreen.kt
@@ -0,0 +1,98 @@
+package com.example.composenewsapp.presentation.home
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.sp
+import androidx.paging.compose.LazyPagingItems
+import com.example.composenewsapp.R
+import com.example.composenewsapp.domain.model.Article
+import com.example.composenewsapp.presentation.Dimens.MediumPadding1
+import com.example.composenewsapp.presentation.common.ArticlesList
+import com.example.composenewsapp.presentation.common.SearchBar
+import com.example.composenewsapp.presentation.navgraph.Route
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HomeScreen(
+    articles: LazyPagingItems<Article>,
+    navigate:(String) -> Unit
+) {
+
+    val titles by remember {
+        derivedStateOf {
+            if (articles.itemCount > 10) {
+                articles.itemSnapshotList.items
+                    .slice(IntRange(start = 0, endInclusive = 9))
+                    .joinToString(separator = " \uD83D\uDFE5 ") { it.title }
+            } else {
+                ""
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = MediumPadding1)
+            .statusBarsPadding(),
+
+    ) {
+        Text(
+            text = "Google News",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = MediumPadding1)
+                .basicMarquee(), fontSize = 30.sp,
+            color = colorResource(id = R.color.placeholder)
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        SearchBar(
+            modifier = Modifier
+                .padding(horizontal = MediumPadding1)
+                .fillMaxWidth(),
+            text = "",
+            readOnly = true,
+            onValueChange = {},
+            onSearch = {},
+            onClick = {
+                navigate(Route.SearchScreen.route)
+            }
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        Text(
+            text = titles, modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = MediumPadding1)
+                .basicMarquee(), fontSize = 12.sp,
+            color = colorResource(id = R.color.placeholder)
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+        ArticlesList(
+            modifier = Modifier.padding(horizontal = MediumPadding1),
+            articles = articles,
+            onClick = {
+                navigate(Route.DetailScreen.route)
+            }
+        )
+    }
+}
--- app/src/main/java/com/example/composenewsapp/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/example/composenewsapp/presentation/navgraph/NavGraph.kt
@@ -1,12 +1,14 @@
 package com.example.composenewsapp.presentation.navgraph
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.composenewsapp.presentation.home.HomeScreen
+import com.example.composenewsapp.presentation.home.HomeViewModel
 import com.example.composenewsapp.presentation.onboarding.OnBoardingScreen
 import com.example.composenewsapp.presentation.onboarding.OnBoardingViewModel
 
@@ -35,7 +37,11 @@ fun NavGraph(
             startDestination = Route.NewsNavigatorScreen.route
         ) {
             composable(route = Route.NewsNavigatorScreen.route) {
-                Text(text = "News Navigator Screen")
+                val viewModel: HomeViewModel = hiltViewModel()
+                val articles = viewModel.news.collectAsLazyPagingItems()
+                HomeScreen(
+                    articles = articles,
+                    navigate = {})
             }
             composable(route = Route.SearchScreen.route) {
 
